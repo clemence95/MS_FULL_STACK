7b9b5d6857823f8d49727e39168a6bc49eec112e
boitelle clémence <boitelleclemence@outlook.fr>
2023-08-31 22:53:09 +0200

    js + php

--- Ms_Full_Stack/php/boucles.php
+++ Ms_Full_Stack/php/boucles.php
@@ -19,16 +19,51 @@
     }
     echo "<br>";
 
-    // Création du tableau
-    $tableau = array();
-
-    // Remplissage du tableau avec les résultats de multiplication
-    for ($i = 0; $i <= 9; $i++) {
-        for ($j = 0; $j <= 9; $j++) {
-            $tableau[$i][$j] = $i * $j;
-        }
-    }
     ?>
-</body>
+
+
+<title>Table de multiplication</title>
+  <style>
+    table {
+      border-collapse: collapse;
+      margin: auto;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <h1>Table de multiplication</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>x</th>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th>4</th>
+        <th>5</th>
+        <th>6</th>
+        <th>7</th>
+        <th>8</th>
+        <th>9</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Boucle pour les lignes -->
+      <?php for ($i = 1; $i <= 9; $i++) { ?>
+        <tr>
+          <th><?php echo $i; ?></th>
+          <!-- Boucle pour les colonnes -->
+          <?php for ($j = 1; $j <= 9; $j++) { ?>
+            <td><?php echo $i * $j; ?></td>
+          <?php } ?>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
 
 </html>
