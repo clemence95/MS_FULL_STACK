4f75d5436f64b41d73bafc884c7a11a9810d2ab0
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-09-04 14:09:07 +0200

    php

---
+++ Ms_Full_Stack/assets/php/banner.php
@@ -0,0 +1,14 @@
+<section class="banner d-flex pt-5" style="width: 100%; position: relative;">
+    <div class="container-fluid my-5 py-4 d-flex justify-content-center align-items-center">
+        <div class="video-container d-flex justify-content-center" style="width: 100%;">
+            <video id="myVideo" class="d-block video rounded-5" autoplay muted loop style="width: 100%;">
+                <source src="/Projet-theDistrict/assets/img-thedistrict/pizza-fond.mp4" type="video/mp4">
+                Votre navigateur ne prend pas en charge la lecture de vidéos HTML5.
+            </video>
+        </div>
+        <form class="d-flex " role="search" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <input class="form-control" type="search" placeholder="Rechercher" aria-label="Search" style="width: 50vh; height: 30px">
+            <button class="btn btn-outline-success" style="width: 20vhvh; height: 30px;" type="submit">Recherche</button>
+        </form>
+    </div>
+</section>
