efa69b0115a607e48aba5b4a5d2739bdea92a820
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-09-07 13:24:50 +0200

    php

--- Ms_Full_Stack/php/details_disc.php
+++ Ms_Full_Stack/php/details_disc.php
@@ -1,14 +1,9 @@
 <?php
-try{
-    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=record', 'admin', 'Afpa1234');
+    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=record', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(Exception $e){
-    echo "Erreur : " . $e->getMessage() . "<br>";
-    echo "N° : " . $e->getCode();
-    die("Fin du script");
-}
-    $requete = $db->prepare("select * from disc");
-    $requete->execute(array($_GET["disc"]));
+
+    $requete = $db->prepare("select * from disc where disc_id=?");
+    $requete->execute(array($_GET["disc_id"]));
     $disc = $requete->fetch(PDO::FETCH_OBJ);
 ?>
 <html>
@@ -18,10 +13,8 @@ try{
 </head>
 <html>
 <body>
-    <?php foreach ($tableau as $disc): ?>
     Disc N° <?= $disc->disc_id ?>
     Disc name <?= $disc->disc_name ?>
     Disc year <?= $disc->disc_year ?>
-    <?php endforeach; ?>
 </body>
 </html>
