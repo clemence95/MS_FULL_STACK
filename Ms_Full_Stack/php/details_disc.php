4fb4cc14468c9f81f7ec585fc6e1cb11fd655f9d
boitelle clémence <boitelleclemence@outlook.fr>
2023-09-08 18:59:53 +0200

    php

--- Ms_Full_Stack/php/details_disc.php
+++ Ms_Full_Stack/php/details_disc.php
@@ -32,8 +32,8 @@
     <?php
     // Informations de connexion à la base de données
     $serveur = 'localhost';
-    $utilisateur = 'admin';
-    $motDePasse = 'Afpa1234';
+    $utilisateur = 'root';
+    $motDePasse = '';
     $baseDeDonnees = 'record';
 
     try {
