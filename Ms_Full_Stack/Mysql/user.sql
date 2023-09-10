e2d33e2581345eaf7de79852291c14d119e98d71
boitelle clémence <boitelleclemence@outlook.fr>
2023-09-10 16:05:39 +0200

    .

--- Ms_Full_Stack/Mysql/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
