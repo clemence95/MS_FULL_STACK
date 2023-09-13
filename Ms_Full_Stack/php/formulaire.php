e2c1ff78e59ce959df1ba2c08d96c3504add5109
boitelle clémence <boitelleclemence@outlook.fr>
2023-09-13 17:28:10 +0200

    php

--- Ms_Full_Stack/php/formulaire.php
+++ Ms_Full_Stack/php/formulaire.php
@@ -1,64 +1,25 @@
 <!DOCTYPE html>
-<html lang="fr">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TheDestrit</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lugrasimo&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Inscription</title>
 </head>
-
 <body>
-    <nav>
-        <?php
-        include '../assets/php/nav.php';
-        ?>
-    </nav>
-    <?php
-    include '../assets/php/banner.php';
-    ?>
-    <form class=" row g-3 needs-validation" novalidate method="post" action="/Ms_Full_Stack//assets//php//traitement.php">
-        <div class="col-md-3 mb-4">
-            <input type="text" class="form-control" placeholder="Prénom" aria-label="prenom" id="validationCustom01" name="prenom" required>
-            <div class="invalid-feedback">
-                Vous devez saisir votre Prénom
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <input type="text" class="form-control" placeholder="Nom" aria-label="nom" id="validationCustom02" name="nom" required>
-            <div class="invalid-feedback">
-                Vous devez saisir votre Nom
-            </div>
-        </div>
-            <div class="col-md-4 mb-4">
-                <input type="email" class="form-control" id="inputEmail4" placeholder="Email" name="email" required>
-                <div class="invalid-feedback">
-                    Saisir une adresse email valide
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <input type="tel" class="form-control" placeholder="Téléphone" id="exampleInputPassword1" name=telephone required>
-                <div class="invalid-feedback">
-                    Saisir votre numéro de téléphone
-                </div>
-            </div>
-        <div class="col-md-10">
-            <button type="submit" class="btn btn-primary" id="valide">Valider</button>
-        </div>
+    <h2>Inscription</h2>
+    <form action="/Ms_Full_Stack/php/traitement_inscription.php" method="post">
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" required><br><br>
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" name="prenom" required><br><br>
+
+        <label for="email">Adresse e-mail :</label>
+        <input type="email" name="email" required><br><br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" required><br><br>
+
+        <input type="submit" value="S'inscrire">
     </form>
-
-    <footer>
-        <?php
-        include '../assets/php/footer.php';
-        ?>
-    </footer>
-    <script src="/Ms_Full_Stack/assets/script.js/script.js"></script>
 </body>
-
 </html>
+
--- Ms_Full_Stack/php/traitement_inscription.php
+++ Ms_Full_Stack/php/traitement_inscription.php
@@ -1,52 +1,31 @@
 <?php
-// Connexion à la base de données (à adapter selon votre configuration)
-$servername = "localhost";
-$username = "admin";
-$password = "Afpa1234";
-$dbname = "The_district";
+// Connexion à la base de données
+$db = new PDO('mysql:host=localhost;charset=utf8;dbname=record', 'root', '');
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Configuration supplémentaire de PDO si nécessaire
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // Récupération des données du formulaire
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $adresse_mail = $_POST['adresse_mail'];
-    $mot_de_passe = $_POST['mot_de_passe'];
-    
-    // Vérification de l'adresse e-mail unique
-    $sql = "SELECT * FROM user WHERE adresse_mail = :adresse_mail";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':adresse_mail', $adresse_mail);
-    $stmt->execute();
-    
-    if ($stmt->rowCount() > 0) {
-        die("L'adresse e-mail est déjà utilisée. Veuillez en choisir une autre.");
-    }
-    
-    // Hachage du mot de passe (à améliorer pour une sécurité optimale)
-    $mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_DEFAULT);
-    
-    // Insertion des données dans la table
-    $sql = "INSERT INTO user (nom, prenom, adresse_mail, mot_de_passe) VALUES (:nom, :prenom, :adresse_mail, :mot_de_passe)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':nom', $nom);
-    $stmt->bindParam(':prenom', $prenom);
-    $stmt->bindParam(':adresse_mail', $adresse_mail);
-    $stmt->bindParam(':mot_de_passe', $mot_de_passe_hache);
-    
-    if ($stmt->execute()) {
-        echo "Inscription réussie !";
-    } else {
-        echo "Erreur : " . $stmt->errorInfo()[2];
-    }
-    
-    // Fermeture de la connexion à la base de données
-    $conn = null;
-} catch(PDOException $e) {
-    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+// Récupération des données du formulaire
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$email = $_POST['email'];
+$password = $_POST['password']; // Changement de nom du champ
+
+// Hasher le mot de passe
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+// Insérer l'utilisateur dans la table
+$sql = "INSERT INTO user (nom, prenom, email, mot_de_passe) VALUES (:nom, :prenom, :email, :mot_de_passe)";
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindParam(':nom', $nom);
+$stmt->bindParam(':prenom', $prenom);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':mot_de_passe', $password_hash); // Changement de nom du champ
+
+if ($stmt->execute()) {
+    echo "Inscription réussie !";
+} else {
+    echo "Une erreur est survenue.";
 }
 ?>
+
+
 
