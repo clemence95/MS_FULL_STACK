91e3fe75c39004d35acd8ac656645424fbcaa63e
boitelle clémence <boitelleclemence@outlook.fr>
2023-09-20 16:48:19 +0200

    .

--- Ms_Full_Stack/Projet_District/form_traitement_demande.php
+++ Ms_Full_Stack/Projet_District/form_traitement_demande.php
@@ -13,6 +13,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 
+// ...
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
@@ -22,19 +23,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telephone = $_POST["telephone"];
     $demande = $_POST["demande"];
 
+    // Obtenir la date actuelle au format MySQL (YYYY-MM-DD HH:MM:SS)
+    $date_demande = date("Y-m-d H:i:s");
+
     // Préparer et exécuter la requête SQL pour ajouter la demande dans la base de données
-    $sql = "INSERT INTO demandes (prenom, nom, email, telephone, demande) VALUES (:prenom, :nom, :email, :telephone, :demande)";
+    $sql = "INSERT INTO demandes (prenom, nom, email, telephone, demande, date_demande) VALUES (:prenom, :nom, :email, :telephone, :demande, :date_demande)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':prenom', $prenom);
     $stmt->bindParam(':nom', $nom);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':telephone', $telephone);
     $stmt->bindParam(':demande', $demande);
+    $stmt->bindParam(':date_demande', $date_demande);
 
     if ($stmt->execute()) {
         // La demande a été ajoutée avec succès à la base de données
         // Vous pouvez rediriger l'utilisateur vers une page de confirmation ou afficher un message de succès
-        header("Location: confirmation.php"); // Redirection vers une page de confirmation
+        header("Location: confirmation_demande.php"); // Redirection vers une page de confirmation
         exit();
     } else {
         // Erreur lors de l'ajout de la demande
