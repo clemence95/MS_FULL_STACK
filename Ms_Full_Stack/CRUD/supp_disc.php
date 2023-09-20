bea53d625a65863264f7b596e473fdd79c0ab179
boitelle clémence <boitelleclemence@outlook.fr>
2023-09-20 15:07:25 +0200

    CRUD supp_disc.php

--- Ms_Full_Stack/CRUD/supp_disc.php
+++ Ms_Full_Stack/CRUD/supp_disc.php
@@ -2,26 +2,40 @@
 // Inclure le fichier de connexion à la base de données
 include("connexion.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupération de l'ID du disque à supprimer
-    $disc_id = $_POST['disc_id'];
+// Vérifier si un identifiant de disque est spécifié dans l'URL
+if (isset($_GET['disc_id'])) {
+    // Récupérer l'identifiant de disque à supprimer depuis l'URL
+    $discIdToDelete = $_GET['disc_id'];
 
-    // Requête SQL pour supprimer le disque de la base de données
-    $sql = "DELETE FROM disc WHERE disc_id = :disc_id";
+    // Requête SQL pour supprimer le disque en fonction de son identifiant
+    $sqlDelete = "DELETE FROM disc WHERE disc_id = :disc_id";
+    $stmt = $conn->prepare($sqlDelete);
 
-    try {
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(":disc_id", $disc_id);
-        $stmt->execute();
+    // Liaison de l'identifiant du disque en tant que paramètre
+    $stmt->bindParam(':disc_id', $discIdToDelete, PDO::PARAM_INT);
 
-        // Redirection vers la page "index_disc.php" après la suppression
+    // Exécution de la requête
+    if ($stmt->execute()) {
+        // Rediriger vers la page principale ou une autre page de confirmation
         header("Location: index_disc.php");
         exit();
-    } catch (PDOException $e) {
-        echo "Erreur : " . $e->getMessage();
+    } else {
+        // Gérer les erreurs de suppression ici
+        echo "Erreur lors de la suppression du disque.";
     }
+} else {
+    // Gérer le cas où aucun identifiant de disque n'est spécifié dans l'URL
+    echo "Aucun identifiant de disque spécifié pour la suppression.";
 }
-
-// Fermeture de la connexion à la base de données
-$conn = null;
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Supprimer un Vinyle</title>
+</head>
+<body>
+    <h1>Supprimer un Vinyle</h1>
+    <!-- Vous pouvez ajouter un formulaire ou des informations de confirmation ici si nécessaire -->
+</body>
+</html>
