e6d9f2a92e666e735c58e770e9ce3e36d39b611f
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-10-05 16:12:26 +0200

    .

--- Ms_Full_Stack/Projet_District/banner.php
+++ Ms_Full_Stack/Projet_District/banner.php
@@ -2,13 +2,13 @@
     <div class="container-fluid my-5 py-4 d-flex justify-content-center align-items-center">
         <div class="video-container d-flex justify-content-center" style="width: 100%;">
             <video id="myVideo" class="d-block video rounded-5" autoplay muted loop style="width: 100%;">
-                <source src="./assets//pizza-fond.mp4" type="video/mp4">
+                <source src="./assets/pizza-fond.mp4" type="video/mp4">
                 Votre navigateur ne prend pas en charge la lecture de vidéos HTML5.
             </video>
         </div>
-        <form class="d-flex col-12 col-sm-6 mb-3 mb-sm-0 " role="search" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-            <input class="form-control" type="search" placeholder="Rechercher" aria-label="Search" style="width: 50vh; height: 30px">
-            <button class="btn btn-outline-success" style="width: 20vhvh; height: 30px;" type="submit">Recherche</button>
+        <form action="search.php" method="GET" class="d-flex col-12 col-sm-6 mb-3 mb-sm-0 " role="search" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <input class="form-control" type="search" placeholder="Rechercher" aria-label="Search" name="query" style="width: 50vh; height: 30px">
+            <button class="btn btn-outline-success" style="width: 20vh; height: 30px;" type="submit">Recherche</button>
         </form>
     </div>
 </section>
